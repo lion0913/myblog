4aa399c335a08f573fd950554857ae6815f8d089
dajung <ekwjd990913@gmail.com>
2022-06-28 11:30:06 +0900

    gnb: Head 추가 + navigation별 화면 페이지 구성

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import { Link } from "gatsby";
+import React from "react";
+import Head from "../components/Head"
+
+function AboutPage() {
+    return (
+        <main>
+            <title>자기소개</title>
+            <Head />
+            <h1>자기소개 페이지입니다.</h1>
+        </main>
+    )
+}
+
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
-import * as React from "react"
+import { Link } from "gatsby";
+import React from "react";
+import Head from "../components/Head"
 
 // markup
-const IndexPage = () => {
+function IndexPage() {
   return (
     <main>
       <title>About Me</title>
+      <Head />
       <h1>About Me</h1>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </main>
